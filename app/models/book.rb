f2406e1391b4687c5f2f5e12ad0127c9f2cf0590
Andy Eulogio <andy.eulogio@gmail.com>
2021-09-16 17:58:36 -0500

    Agregado validaciones al modelo

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :genre
   belongs_to :author
+
+  validates :title, presence: true
+  validates :year, presence: true
+  validates :genre, presence: true
+  validates :author, presence: true
 end
